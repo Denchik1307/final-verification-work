124ca4d624b7390289a36ab6232a775303d909d6
Denis <denis13071981@gmail.com>
2022-11-24 00:05:35 +0300

    add programm

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿Console.Clear();
+
+string[] testOne = { "hello", "2", "world", ":-)" };
+string[] testTwo = { "1234", "1567", "-2", "computer science" };
+string[] testThird = { "Russia", "Denmark", "Kazan" };
+string[] manualInputArray = GetArrayFromConsole("Input STRING array split , : ");
+
+Console.WriteLine(manualInputArray.Length);
+
+string[] resultOne = FindStringBySizeInArray(testOne);
+ShowResult(testOne, resultOne);
+
+string[] resultTwo = FindStringBySizeInArray(testTwo);
+ShowResult(testTwo, resultTwo);
+
+string[] resultThird = FindStringBySizeInArray(testThird);
+ShowResult(testThird, resultThird);
+
+string[] resultManualInputArray = FindStringBySizeInArray(manualInputArray);
+ShowResult(manualInputArray, resultManualInputArray);
+
+
+string[] GetArrayFromConsole(string message = "")
+{
+    Console.Write(message);
+    return Console.ReadLine()!.Split(",").ToArray();
+}
+
+void ShowResult(string[] test, string[] result)
+{
+    Console.WriteLine();
+    ShowArray(test);
+    Console.Write("->");
+    ShowArray(result);
+    Console.WriteLine();
+}
+
+string[] FindStringBySizeInArray(string[] str, int length = 3) => str.Select(s => s).Where(s => (s.Length <= length)).ToArray();
+
+void ShowArray(string[] inputArray)
+{
+    if (inputArray.Length > 0 && inputArray[0].Length != 0)
+    {
+        Console.Write($"[\"{String.Join("\",\"", inputArray)}\"]");
+    }
+    else
+    {
+        Console.Write("[]");
+    }
+}
